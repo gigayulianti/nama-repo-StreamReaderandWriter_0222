f7a0aac3eef8df6371170eb505f6e85e856ea598
gigayulianti <yulianti2@gmail.com>
2026-06-14 01:16:12 +0700

    commit 20

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -46,4 +46,10 @@ int main()
             cout << baris << endl;
         }
         //menutup file jika telah selesai
-        
+        infile.close(); 
+    }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    return 0;
+
+}
