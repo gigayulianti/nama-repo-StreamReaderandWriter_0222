0ac9fe8ae4bf5cbdb576bd98857461efca5d410c
gigayulianti <yulianti2@gmail.com>
2026-06-14 01:08:07 +0700

    commit 10

--- ManualException.cpp
+++ ManualException.cpp
@@ -6,4 +6,11 @@ int main()
     //mambuat isi dari try untuk pengujian error
     try
     {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integar maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    //kondisi jika throw yang bertipe data integar
+    catch (int a)
+    {
         
