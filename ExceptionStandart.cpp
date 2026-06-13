be903c3d9989c0aa392262eccc0f7ddbafad678b
gigayulianti <yulianti2@gmail.com>
2026-06-14 01:03:33 +0700

    commit 1

---
+++ ExceptionStandart.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+
+int main()
+{
+    cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        
