abeb5070a26a5745165962b29437b78e05932342
gigayulianti <yulianti2@gmail.com>
2026-06-14 01:11:54 +0700

    commit 13

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -6,4 +6,14 @@ using namespace std;
 int main()
 {
 
+    //deklarasin variabel baris untuk menyimpan data string 
+    string baris;
+
+    //membuat file dalam mode menulis 
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
