58a50fd26174c6d0fe0ac8c0d8be21993f80b276
gigayulianti <yulianti2@gmail.com>
2026-06-14 01:05:06 +0700

    commit 4

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -9,4 +9,19 @@ int main()
     //deklarasi variable baris dan NamaFile
     string baris, NamaFile;
 
-    
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file :";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' umtuk keluar" << endl;
+
+    //looping untuk menulis isi file
+    while (true)
+    {
+
+        
